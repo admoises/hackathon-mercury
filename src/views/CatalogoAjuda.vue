4fc9198af1acfecf663aea4f3663c3674fb1f7f5
Nael Melo Campelo <nael.nmc@gmail.com>
2021-10-02 12:09:09 -0300

    Implementando  Wireframe em Catalogo

--- src/views/CatalogoAjuda.vue
+++ src/views/CatalogoAjuda.vue
@@ -1,13 +1,59 @@
 <template>
   <div class="catalogo-de-ajuda">
-      <h2>Catalogo de Ajuda</h2>
+      
+      
+<template>
+  <v-app id="inspire">
+
+
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      temporary
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row>
+          <template v-for="n in 4">
+            <v-col
+              :key="n"
+              class="mt-2"
+              cols="12"
+            >
+              <strong>Conteudo tipo {{ n }}</strong>
+            </v-col>
+
+            <v-col
+              v-for="j in 6"
+              :key="`${n}${j}`"
+              cols="6"
+              md="2"
+            >
+              <v-sheet height="150"></v-sheet>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+
+
+
+
+
+
   </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    data: () => ({ drawer: null }),
+  }
 </script>
 
 <style scope>
